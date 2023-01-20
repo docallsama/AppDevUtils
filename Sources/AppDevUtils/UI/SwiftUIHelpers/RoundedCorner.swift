78f082c39742df67c59570b8dbf6f78ff7e4af1e
Igor Tarasenko <tarasenko.igor.v@gmail.com>
2023-01-20 16:45:31 +0100

    Add rounded corner shape and view extension for SwiftUI
    This commit adds a new `RoundedCornersShape` struct to provide a custom `Shape` implementation which can be used to add rounded corners to views in SwiftUI. Additionally, an extension on the `View` protocol is added, providing an easy way to apply this shape with a given radius and set of corners.

--- Sources/AppDevUtils/UI/SwiftUIHelpers/RoundedCorner.swift
+++ Sources/AppDevUtils/UI/SwiftUIHelpers/RoundedCorner.swift
@@ -1,9 +1,8 @@
 import SwiftUI
 
-// MARK: - RoundedCorner
-
 #if os(iOS) || os(tvOS) || os(watchOS)
-  public struct RoundedCorner: Shape {
+  typealias RectCorner = UIRectCorner
+  public struct RoundedCornersShape: Shape {
     var radius: CGFloat = .infinity
     var corners: UIRectCorner = .allCorners
 
@@ -12,10 +11,66 @@ import SwiftUI
       return Path(path.cgPath)
     }
   }
+#elseif os(macOS)
+public struct RectCorner: OptionSet {
+  public let rawValue: Int
 
-  public extension View {
-    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
-      clipShape(RoundedCorner(radius: radius, corners: corners))
-    }
+  public static let topLeft = RectCorner(rawValue: 1 << 0)
+  public static let topRight = RectCorner(rawValue: 1 << 1)
+  public static let bottomRight = RectCorner(rawValue: 1 << 2)
+  public static let bottomLeft = RectCorner(rawValue: 1 << 3)
+
+  public static let allCorners: RectCorner = [.topLeft, topRight, .bottomLeft, .bottomRight]
+
+  public init(rawValue: Int) {
+    self.rawValue = rawValue
   }
+}
+
+struct RoundedCornersShape: Shape {
+  var radius: CGFloat = .zero
+  var corners: RectCorner = .allCorners
+
+  func path(in rect: CGRect) -> Path {
+    var path = Path()
+
+    let p1 = CGPoint(x: rect.minX, y: corners.contains(.topLeft) ? rect.minY + radius : rect.minY)
+    let p2 = CGPoint(x: corners.contains(.topLeft) ? rect.minX + radius : rect.minX, y: rect.minY)
+
+    let p3 = CGPoint(x: corners.contains(.topRight) ? rect.maxX - radius : rect.maxX, y: rect.minY)
+    let p4 = CGPoint(x: rect.maxX, y: corners.contains(.topRight) ? rect.minY + radius : rect.minY)
+
+    let p5 = CGPoint(x: rect.maxX, y: corners.contains(.bottomRight) ? rect.maxY - radius : rect.maxY)
+    let p6 = CGPoint(x: corners.contains(.bottomRight) ? rect.maxX - radius : rect.maxX, y: rect.maxY)
+
+    let p7 = CGPoint(x: corners.contains(.bottomLeft) ? rect.minX + radius : rect.minX, y: rect.maxY)
+    let p8 = CGPoint(x: rect.minX, y: corners.contains(.bottomLeft) ? rect.maxY - radius : rect.maxY)
+
+    path.move(to: p1)
+    path.addArc(tangent1End: CGPoint(x: rect.minX, y: rect.minY),
+                tangent2End: p2,
+                radius: radius)
+    path.addLine(to: p3)
+    path.addArc(tangent1End: CGPoint(x: rect.maxX, y: rect.minY),
+                tangent2End: p4,
+                radius: radius)
+    path.addLine(to: p5)
+    path.addArc(tangent1End: CGPoint(x: rect.maxX, y: rect.maxY),
+                tangent2End: p6,
+                radius: radius)
+    path.addLine(to: p7)
+    path.addArc(tangent1End: CGPoint(x: rect.minX, y: rect.maxY),
+                tangent2End: p8,
+                radius: radius)
+    path.closeSubpath()
+
+    return path
+  }
+}
 #endif
+
+public extension View {
+  func roundedCorners(radius: CGFloat, corners: RectCorner) -> some View {
+    clipShape(RoundedCornersShape(radius: radius, corners: corners))
+  }
+}
