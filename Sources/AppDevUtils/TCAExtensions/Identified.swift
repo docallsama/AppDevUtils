d2029953da33b965abed612899dda00660ab03de
Igor Tarasenko <tarasenko.igor.v@gmail.com>
2023-02-08 02:07:23 +0100

    Removes identified type

--- Sources/AppDevUtils/TCAExtensions/Identified.swift
+++ Sources/AppDevUtils/TCAExtensions/Identified.swift
@@ -1,29 +1,5 @@
 import ComposableArchitecture
 
-// MARK: - Identified
-
-public struct Identified<ID: Hashable, Value>: Identifiable {
-  public var id: ID
-  public var value: Value
-
-  public init(id: ID, value: Value) {
-    self.id = id
-    self.value = value
-  }
-}
-
-// MARK: Equatable
-
-extension Identified: Equatable where Value: Equatable {}
-
-// MARK: Hashable
-
-extension Identified: Hashable where Value: Hashable {}
-
-// MARK: Codable
-
-extension Identified: Codable where ID: Codable, Value: Codable {}
-
 public extension Array {
   func identifiedByIndex() -> IdentifiedArrayOf<Identified<Int, Element>> {
     IdentifiedArray(uniqueElements: enumerated().map(Identified.init(id:value:)))
